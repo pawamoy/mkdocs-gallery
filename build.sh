1beda6df0cd542d67cdd5aeddf065a3affe1dd37
Timothée Mazzucotelli <pawamoy@pm.me>
2023-09-27 15:40:46 +0200

    Faster builds

--- build.sh
+++ build.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env bash
 
-rm -rf site
-mkdocs build
-mkdir -p site/themes
-
-mkdir logs &>/dev/null
+echo "Preparing environments"
 for theme in themes/*; do
-    if (
+    (
         cd "${theme}"
         if [ ! -d .venv ]; then
-            echo "Preparing environment for ${theme}"
             python -m venv .venv
-            .venv/bin/pip install mkdocs &>/dev/null
             .venv/bin/pip install $(mkdocs get-deps) &>/dev/null
         fi
-        echo "Building ${theme}"
-        .venv/bin/mkdocs build &>../../logs/${theme##*/}.txt
-    ); then
-        mv "${theme}/site" "site/themes/${theme##*/}"
-    else
-        echo "FAILED!"
-    fi
+    ) &
+done
+
+wait
+
+echo "Building main documentation"
+rm -rf site
+mkdocs build
+mkdir site/themes
+mkdir logs &>/dev/null
+
+for theme in themes/*; do
+    echo "Building ${theme}"
+    (
+        cd "${theme}"
+        if .venv/bin/mkdocs build &>../../logs/${theme##*/}.txt; then
+            mv site "../../site/themes/${theme##*/}"
+        else
+            echo "FAILED!"
+        fi
+    )
 done
