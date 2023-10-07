5b1d9dd541e844bd7b471369902ceaa23585d4c2
Oleh Prypin <oleh@pryp.in>
2023-10-07 23:23:47 +0200

    refactor: More widely apply Jinja templates for specimen

--- build.py
+++ build.py
@@ -7,7 +7,6 @@ import venv
 from dataclasses import dataclass
 from multiprocessing import Pool
 from pathlib import Path
-from textwrap import dedent
 
 import httpx
 import yaml
@@ -22,11 +21,12 @@ class Theme:
     mkdocs_id: str
     url: str = ""
     pypi_id: str = ""
+    builtin: bool = False
 
 
-builtin_themes = [
-    Theme(name="MkDocs", mkdocs_id="mkdocs"),
-    Theme(name="ReadTheDocs", mkdocs_id="readthedocs"),
+_builtin_themes = [
+    Theme(name="MkDocs", mkdocs_id="mkdocs", builtin=True),
+    Theme(name="ReadTheDocs", mkdocs_id="readthedocs", builtin=True),
 ]
 
 
@@ -53,99 +53,44 @@ def get_themes() -> list[Theme]:
                     themes.append(
                         Theme(name=f"{project['name']} - {theme.title()}", url=url, pypi_id=pypi_id, mkdocs_id=theme)
                     )
-    return sorted(themes, key=lambda theme: theme.name.lower())
+    return _builtin_themes + sorted(themes, key=lambda theme: theme.name.lower())
+
+
+# Copy files and expand Jinja templates.
+def _prepare_site(src_dir: Path, dest_dir: Path, themes: list[Theme], theme: Theme | None = None) -> None:
+    jinja = Environment(autoescape=False)
+    dest_dir.mkdir(parents=True, exist_ok=True)
+
+    for src_path in src_dir.rglob("*"):
+        if not src_path.is_file():
+            continue
+        dest_path = dest_dir.joinpath(src_path.relative_to(src_dir))
+        dest_path.parent.mkdir(parents=True, exist_ok=True)
+        if src_path.suffix in (".md", ".yml"):
+            content = src_path.read_text()
+            content = jinja.from_string(content).render(themes=themes, theme=theme)
+            dest_path.write_text(content)
+        else:
+            shutil.copyfile(src_path, dest_path)
 
 
 # Prepare each theme (docs directory and configuration file).
 def prepare_themes(themes: list[Theme]) -> None:
-    jinja = Environment(autoescape=False)
-    specimen_dir = Path("specimen").absolute()
-    for theme in builtin_themes + themes:
+    specimen_dir = Path("templates", "specimen")
+    for theme in themes:
         # Copy specific directory, or default to specimen.
         theme_dir = Path("themes", theme.mkdocs_id)
-        theme_conf_dir = Path("themes_conf", theme.mkdocs_id)
+        theme_conf_dir = Path("templates", "themes", theme.mkdocs_id)
         if not theme_conf_dir.exists():
             theme_conf_dir = specimen_dir
         shutil.copytree(theme_conf_dir, theme_dir, dirs_exist_ok=True)
 
-        # Update mkdocs.yml.
-        mkdocs_yml = theme_dir / "mkdocs.yml"
-        mkdocs_yml.write_text(
-            jinja.from_string(mkdocs_yml.read_text()).render(theme=theme.mkdocs_id, site_name=theme.name, themes=themes)
-        )
-
-        # Update docs/index.md.
-        index_md = theme_dir / "docs" / "index.md"
-        index_md.write_text(jinja.from_string(index_md.read_text()).render(theme=theme, themes=themes))
+        _prepare_site(specimen_dir, theme_dir, themes, theme=theme)
 
 
 # Prepare the main documentation site.
 def prepare_main(themes: list[Theme]) -> None:
-    # Our main MkDocs configuration.
-    main_conf = dedent(
-        """
-        site_name: Gallery
-        site_url: https://pawamoy.github.io/mkdocs-gallery
-        theme:
-          name: material
-          logo: assets/logo.png
-          palette:
-          - media: "(prefers-color-scheme: light)"
-            scheme: default
-            primary: blue
-            toggle:
-              icon: material/brightness-7 
-              name: Switch to dark mode
-          - media: "(prefers-color-scheme: dark)"
-            scheme: slate
-            primary: blue
-            toggle:
-              icon: material/brightness-4
-              name: Switch to light mode
-        markdown_extensions:
-        - attr_list
-        - toc:
-            permalink: true
-        """
-    ).strip()
-
-    Path("mkdocs.yml").write_text(main_conf)
-
-    # The home page.
-    index_contents = dedent(
-        """
-        ---
-        hide:
-        - navigation
-        ---
-
-        # Welcome to our gallery of MkDocs themes!
-
-        <style>
-        article img {
-            -webkit-filter: drop-shadow(0px 16px 10px rgba(100,100,100,0.6));
-            -moz-filter: drop-shadow(0px 16px 10px rgba(100,100,100,0.6));
-            -ms-filter: drop-shadow(0px 16px 10px rgba(100,100,100,0.6)); 
-            -o-filter: drop-shadow(0px 16px 10px rgba(100,100,100,0.6));
-            filter: drop-shadow(0px 16px 10px rgba(100,100,100,0.6));
-        }
-        </style>
-        """
-    ).strip()
-
-    Path("docs").mkdir(parents=True, exist_ok=True)
-    with open("docs/index.md", "w") as index_page:
-        print(index_contents, file=index_page)
-        print("\n## Built-in themes\n", file=index_page)
-        for theme in builtin_themes:
-            img = f"![{theme.name}](assets/img/{theme.mkdocs_id}.png)"
-            link = f'[{img}](themes/{theme.mkdocs_id}){{ title="Click to browse!" }}'
-            print(f"### {theme.name}\n\n{link}\n\n---\n\n", file=index_page)
-        print("\n## Third-party themes\n", file=index_page)
-        for theme in themes:
-            img = f"![{theme.name}](assets/img/{theme.mkdocs_id}.png)"
-            link = f'[{img}](themes/{theme.mkdocs_id}){{ title="Click to browse!" }}'
-            print(f"### {theme.name}\n\n{link}\n\n---\n\n", file=index_page)
+    _prepare_site(Path("templates", "main"), Path("."), themes)
 
 
 # Create virtualenvs and install dependencies.
@@ -216,18 +161,19 @@ def build_themes(themes: list[Theme]) -> None:
     if not opts.build_themes:
         print("Skipping themes building")
     else:
-        shutil.rmtree("site", ignore_errors=True)
         logs_dir = Path("logs")
         logs_dir.mkdir(exist_ok=True)
-        Path("site", "themes").mkdir(parents=True, exist_ok=True)
+        shutil.rmtree(Path("site", "themes"), ignore_errors=True)
+        Path("site", "themes").mkdir(parents=True)
 
         def _build_theme(theme: Theme) -> None:
-            theme_dir = Path("themes", theme.mkdocs_id)
+            theme_dir = Path("themes", theme.mkdocs_id).absolute()
+            dest_dir = Path("site", "themes", theme.mkdocs_id).absolute()
             print(f"Building {theme.name}")
             with logs_dir.joinpath(f"{theme.mkdocs_id}.txt").open("w") as logs_file:
                 try:
                     subprocess.run(
-                        [".venv/bin/mkdocs", "build"],
+                        [theme_dir.joinpath(".venv", "bin", "mkdocs"), "build", "-d", dest_dir],
                         stdout=logs_file,
                         stderr=logs_file,
                         check=True,
@@ -236,8 +182,6 @@ def build_themes(themes: list[Theme]) -> None:
                     )
                 except subprocess.CalledProcessError:
                     print("FAILED!")
-                else:
-                    shutil.move(theme_dir / "site", f"site/themes/{theme.mkdocs_id}")
 
         for theme in themes:
             _build_theme(theme)
@@ -247,7 +191,7 @@ def build_themes(themes: list[Theme]) -> None:
     else:
         print("Taking screenshots")
         Path("docs", "assets", "img").mkdir(parents=True, exist_ok=True)
-        for theme in themes:
+        for theme in tqdm(themes):
             try:
                 shot_scraper(
                     [f"site/themes/{theme.mkdocs_id}/index.html", "-o", f"docs/assets/img/{theme.mkdocs_id}.png"]
@@ -267,7 +211,7 @@ def main() -> None:
     themes = get_themes()
     prepare_themes(themes)
     prepare_main(themes)
-    build_themes(builtin_themes + themes)
+    build_themes(themes)
     build_main()
 
 
